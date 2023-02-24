e4cabbbbe71930aa71328e64c7e9e968556808de
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-02-24 23:12:43 +0530

    Update Virtual Mouse and Keyboard.py

--- Virtual Mouse and Keyboard.py
+++ Virtual Mouse and Keyboard.py
@@ -1,5 +1,9 @@
 import cv2
-import mediapipe as mp
+
 import math
 import time
 cap=cv2.VideoCapture(0)
+while True:
+    _,frame=cap.read()
+    cv2.imshow('Virtual Mouse',frame)
+    cv2.waitKey(1)
